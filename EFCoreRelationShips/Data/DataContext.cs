b6a8f99496b03879c9adaa6e43061f74cb5c8e08
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-07 17:28:49 +0300

    dbcontext

---
+++ EFCoreRelationShips/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationShips.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+  
+
+    }
+}
