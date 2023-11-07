20f88ca5f11ed582263fcf36bc4a0c16b5089c85
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-07 17:46:45 +0300

    character model

---
+++ EFCoreRelationShips/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreRelationShips
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = "Knight";
+        public User User { get; set; } = null!;
+    }
+}
--- EFCoreRelationShips/User.cs
+++ EFCoreRelationShips/User.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+
+        public List<Character> Characters { get; set; } = null!;
+
     }
 }
