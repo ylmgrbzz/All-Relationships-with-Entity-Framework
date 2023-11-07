f6d94b7f7363ec0abd403eeede2cf2c38b7ab770
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-07 17:31:03 +0300

    user model

--- EFCoreRelationShips/Data/DataContext.cs
+++ EFCoreRelationShips/Data/DataContext.cs
@@ -5,7 +5,8 @@ namespace EFCoreRelationShips.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-  
+        
+        public DbSet<User> Users { get; set; } = null!;
 
     }
 }
---
+++ EFCoreRelationShips/User.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCoreRelationShips
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+    }
+}
